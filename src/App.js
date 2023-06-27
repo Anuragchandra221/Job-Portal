a1a3dd1d7688822d334e4e2a84b5b0b765779439
Anuragchandra221 <pothaluranurag2002@gmail.com>
2023-06-27 21:03:58 +0530

    basic project structure defined

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import Login from './Pages/Login';
+import SignUp from './Pages/SignUp';
+import Home from './Pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/sign-up" element={<SignUp/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
